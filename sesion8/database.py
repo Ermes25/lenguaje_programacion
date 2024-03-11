f4806f3b1a414e1349cc3e82ab7e3eed1dd46bd3
Ermes25 <ermes252020@gmail.com>
2024-03-11 13:43:25 -0600

    se actualizo la base de datos, Hubo Errores en los atributos connector pero ya fueron arreglados

--- sesion8/database.py
+++ sesion8/database.py
@@ -7,38 +7,40 @@ class MYSQLDB:
         self.user = user
         self.pw = password
         self.db = database   
-        self.Connection = None     
+        self.connection = None     
                 
     def connect(self):
         try:
-            if(self.connect == None):
-                self.Connection = mysql.connector.connect(
+            if(self.connection == None):
+                self.connection = mysql.connector.connect(
                     host = self.host,
                     user = self.user,
                     password = self.pw,
                     database = self.db
                 )
+                os.system("color a2")
                 print("Congratulations")
         except mysql.connector.Error as error:
             print("Error mientras se estaba conectando {}".format(error))
     def disconect(self):
         try:
-             if self.Connection:
-                    self.Connection.close
-                    self.Connection = None
-        except mysql.connector.error as Error:
+             if self.connection:
+                    self.connection.close
+                    self.connection = None
+        except mysql.connector.Error as error:
             print("Error")
-    def execute_query(self,query,params = None):
+    def execute_query(self,query,params=None):
         try:
-            cursor = self.Connection.cursor
+            cursor = self.connection.cursor()
             cursor.execute(query,params)
             result = cursor.fetchall()
             return result
         except mysql.connector.Error as err:
-            print(f"error: {err}")
-db =MYSQLDB("localhost","root","","testlp")
+            print(f"Error: {err}")
+db =MYSQLDB("localhost","root","","lacteos_sa")
 print("conectado")
 
 db.connect()
-categorias = db.execute_query("select * from categorias")
-print(categorias)
+categoria = db.execute_query("select * from categoria")
+for reg in categoria:
+    print(categoria)
