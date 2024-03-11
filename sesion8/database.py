eebec2d777f4dfb487a633c5e6933c57f7e759b9
Ermes25 <ermes252020@gmail.com>
2024-03-11 13:59:17 -0600

    agregacion de comentarios y el uso de cada uno de los metodos

--- sesion8/database.py
+++ sesion8/database.py
@@ -1,14 +1,17 @@
 import mysql.connector
+#la importancion de la libreria OS o Operative System fundamenta los usos y las funciones de la Terminal ya sea Windows o Linux
 import os
-
+#La clase MYSQLDB fundamentara el inicio de los parametros de nuestra base de datos que utilizaremos
 class MYSQLDB:
+    #la definicion o el primer metodo lo utilizaremos para la interfaz o el arranque de nuestra base de datos que nos asegura
+    #relacionar no solo en nuestra base datos si no que tambien en otros usuarios que necesitan usarlo 
     def __init__(self,host,user,password,database):
         self.host = host
         self.user = user
         self.pw = password
         self.db = database   
         self.connection = None     
-                
+    #el segundo metodo es una herencia que permite conectar el usuario con la base de datos            
     def connect(self):
         try:
             if(self.connection == None):
@@ -22,6 +25,7 @@ class MYSQLDB:
                 print("Congratulations")
         except mysql.connector.Error as error:
             print("Error mientras se estaba conectando {}".format(error))
+    #El tercer metodo tambien es una herencia que me permite usuario desconectarse desde el editor de Codigo
     def disconect(self):
         try:
              if self.connection:
@@ -29,6 +33,8 @@ class MYSQLDB:
                     self.connection = None
         except mysql.connector.Error as error:
             print("Error")
+    #El cuarto metodo no solo permite presentar las peticion del usuario si no que tambien encapsula los demas metodos permitiendo al usuario
+    #usar solo una peticion de manera automatica reduciendo la cantidad de espera de codigos
     def execute_query(self,query,params=None):
         try:
             cursor = self.connection.cursor()
@@ -37,10 +43,10 @@ class MYSQLDB:
             return result
         except mysql.connector.Error as err:
             print(f"Error: {err}")
-db =MYSQLDB("localhost","root","","lacteos_sa")
+db =MYSQLDB("localhost","root","","testlp")
 print("conectado")
 
 db.connect()
-categoria = db.execute_query("select * from categoria")
-for reg in categoria:
-    print(categoria)
+categorias = db.execute_query("select * from categorias")
+for reg in categorias:
+    print(categorias)
